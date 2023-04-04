15ea6bafa19036e8d007a0b735ec857940565160
Nazaruk-D <nazaruk-dima@mail.ru>
2023-04-04 19:25:49 +0300

    init: redux, axios, sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return <div className="xxx">Hello, World</div>;
+    return <div>Hello, World</div>;
 }
 
 export default App;
