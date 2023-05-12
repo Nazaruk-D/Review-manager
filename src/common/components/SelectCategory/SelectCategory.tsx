b5c855afb1def571be3cddcc8876e7662b7f5513
Nazaruk-D <nazaruk-dima@mail.ru>
2023-05-12 09:35:21 +0300

    feat: validate SendCommentForm

--- src/common/components/SelectCategory/SelectCategory.tsx
+++ src/common/components/SelectCategory/SelectCategory.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { FormikValues } from 'formik';
 import { CATEGORIES } from '../../constants/constants';
+import { Category } from '../../../enums/category';
 
 type SelectCategoryPropsType = {
     formik: FormikValues;
@@ -16,7 +17,7 @@ const SelectCategory: FC<SelectCategoryPropsType> = ({ formik }) => {
         <FormControl fullWidth>
             <InputLabel id="demo-simple-select-label">{t('category')}</InputLabel>
             <Select labelId="demo-simple-select-label" {...formik.getFieldProps('category')}>
-                {CATEGORIES.map((category: string) => (
+                {CATEGORIES.filter((category) => category !== Category.All).map((category: string) => (
                     <MenuItem key={category} value={category}>
                         {tc(category)}
                     </MenuItem>
--- src/features/review/Review/SendCommentForm/SendCommentForm.tsx
+++ src/features/review/Review/SendCommentForm/SendCommentForm.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
-import { Button, CircularProgress, Grid, TextField } from '@mui/material';
+import { Box, Button, CircularProgress, Grid, TextField } from '@mui/material';
 import { Socket } from 'socket.io-client';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { useTranslation } from 'react-i18next';
@@ -12,19 +12,35 @@ type SendCommentFormPropsType = {
 const SendCommentForm: FC<SendCommentFormPropsType> = ({ ws, sendComment }) => {
     const [loading, setLoading] = useState(false);
     const [comment, setComment] = useState('');
+    const [error, setError] = useState('');
     const { t } = useTranslation('translation', { keyPrefix: 'action' });
 
+    const validateComment = (message: string) => {
+        if (!message) {
+            setError('Comment should not be empty');
+        } else if (message.length > 100) {
+            setError('Comment should not exceed 100 characters');
+        } else {
+            setError('');
+        }
+    };
+
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setComment(event.currentTarget.value);
+        const { value } = event.currentTarget;
+        setComment(value);
+        validateComment(value);
     };
 
     const onClickHandler = () => {
-        setLoading(true);
-        sendComment(comment);
+        if (!error) {
+            setLoading(true);
+            sendComment(comment);
+            setComment('');
+        }
     };
 
     const onKeyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter' && !error) {
             event.preventDefault();
             setLoading(true);
             sendComment(comment);
@@ -56,9 +72,17 @@ const SendCommentForm: FC<SendCommentFormPropsType> = ({ ws, sendComment }) => {
                         onChange={onChangeHandler}
                         onKeyDown={onKeyDownHandler}
                     />
+                    {error && <Box sx={{ color: 'red' }}>{error}</Box>}
                 </Grid>
                 <Grid item>
-                    <Button variant="contained" color="primary" disableElevation onClick={onClickHandler} sx={{ width: '125px' }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                        onClick={onClickHandler}
+                        sx={{ width: '125px' }}
+                        disabled={comment.length < 1 || error !== ''}
+                    >
                         {loading ? <CircularProgress size={24} color="inherit" /> : t('send')}
                     </Button>
                 </Grid>
